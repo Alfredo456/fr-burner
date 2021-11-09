eacb6240b4dd7aa88c0e387dfb39d6dd3d6895aa
Alfredo Cañizales <alfredo.casique456@gmail.com>
2021-11-09 17:39:46 -0400

    adapting payloads and table format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export interface StudyElement {
   studyDescription: string;
   patientName: string;
   isBurned: boolean;
+  patientId: string;
 }
 
 export interface StudyElement2 {
@@ -22,27 +23,27 @@ export interface StudyElement2 {
 }
 
 const ELEMENT_DATA: StudyElement[] = [
-  { idStudy: '1', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '2', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '3', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '4', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '5', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '6', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '7', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '8', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '9', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '10', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
+  { idStudy: '1', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '2', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '3', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '4', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '5', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '6', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '7', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '8', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '9', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '10', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
 
-  { idStudy: '11', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '12', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '13', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '14', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '15', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '16', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '17', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '18', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '19', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
-  { idStudy: '20', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true },
+  { idStudy: '11', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '12', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '13', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '14', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '15', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '16', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '17', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '18', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '19', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
+  { idStudy: '20', studyDescription: 'Patologias varias', patientName: 'Alfredo Cañizales', isBurned: true, patientId: '56465654645' },
 ];
 
 const ELEMENT_DATA2: StudyElement2[] = [
@@ -67,8 +68,8 @@ const ELEMENT_DATA2: StudyElement2[] = [
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['idStudy', 'studyDescription', 'patientName', 'options'];
-  displayedColumns2: string[] = ['idStudy', 'studyDescription', 'patientName', 'patientId', 'options'];
+  displayedColumns: string[] = ['patientId','studyDescription', 'patientName', 'options'];
+  displayedColumns2: string[] = ['patientId', 'studyDescription', 'patientName', 'options'];
   dataSource = new MatTableDataSource<StudyElement>();
   dataSource2 = new MatTableDataSource<StudyElement2>();
   // @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -92,13 +93,14 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.dataSource = new MatTableDataSource<StudyElement>(ELEMENT_DATA);
-    this.resultsLength = this.dataSource.data.length;
-    this.dataSource.paginator = this.paginator ? this.paginator : null;
+    // this.dataSource = new MatTableDataSource<StudyElement>(ELEMENT_DATA);
+    // this.resultsLength = this.dataSource.data.length;
+    // this.dataSource.paginator = this.paginator ? this.paginator : null;
 
-    this.dataSource2 = new MatTableDataSource<StudyElement2>(ELEMENT_DATA2);
-    this.resultsLength2 = this.dataSource2.data.length;
-    this.dataSource2.paginator = this.paginator2 ? this.paginator2 : null;
+    // this.dataSource2 = new MatTableDataSource<StudyElement2>(ELEMENT_DATA2);
+    // this.resultsLength2 = this.dataSource2.data.length;
+    // this.dataSource2.paginator = this.paginator2 ? this.paginator2 : null;
+
     // this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
     this.getList();
     this.getCompleteList();
